4bd8ff56cbd16dc0a494a9be1ee4a729e329cd91
Ruben Rodriguez <hola@rubenr.dev>
2022-02-06 17:46:32 +0100

    Fixes in file structure

--- projects/design-system/src/lib/design-system.module.ts
+++ projects/design-system/src/lib/design-system.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
-import { DesignSystemComponent } from './design-system.component';
-
-
+import ButtonComponent from './components/button/button.component';
 
 @NgModule({
-  declarations: [
-    DesignSystemComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    DesignSystemComponent
-  ]
+  declarations: [ButtonComponent],
+  imports: [],
+  exports: [ButtonComponent],
 })
-export class DesignSystemModule { }
+export class DesignSystemModule {}
--- projects/design-system/src/public-api.ts
+++ projects/design-system/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of design-system
  */
 
-export * from './lib/design-system.service';
-export * from './lib/design-system.component';
 export * from './lib/design-system.module';
